b16a933095c93f094ae3595b236285f539602e91
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-01 16:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ function init() {
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
     return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
   })
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementsById("recipe-details-partial").innerHTML)
-  Handlebars.registerPartial('recipeFormPartial', document.getElementsById("recipe-form-partial").innerHTML)
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
   
-  
+  let formTemplate = document.getElementById("recipe-form-template").innerHTML;
+  let template = Handlebars.compile(formTemplate);
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'});
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
