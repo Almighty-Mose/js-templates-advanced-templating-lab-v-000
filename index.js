31d9449bbb4a571bdf2dc9771287ed12299c05a2
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-01 16:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementsById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementsById("recipe-form-partial").innerHTML)
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
